a5ac56deb06512d690a0d5242690cd43aa7dbb12
Somto Okoye <somto@MacBook-Pro-10.local>
2018-03-27 18:50:03 -0500

    create an account component for user and supplier

--- src/app/accounts/createuseraccount/createuseraccount.component.ts
+++ src/app/accounts/createuseraccount/createuseraccount.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CreateuseraccountComponent implements OnInit {
 
   public title: string;
+  public userAccount: any;
 
   constructor() {}
 
