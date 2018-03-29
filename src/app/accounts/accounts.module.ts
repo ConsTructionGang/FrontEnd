d3e5d0571d27bb1ee4645dd3e382d73639948e56
Maya Muralidhar <mmuralidhar@smu.edu>
2018-03-28 22:53:50 -0500

    Added login component from Alex and fixed extra errors

--- src/app/accounts/accounts.module.ts
+++ src/app/accounts/accounts.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CreatesupplieraccountComponent } from './createsupplieraccount/createsupplieraccount.component';
 import { CreateuseraccountComponent } from './createuseraccount/createuseraccount.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
 
-  declarations: [CreatesupplieraccountComponent, CreateuseraccountComponent],
+  declarations: [CreatesupplieraccountComponent, CreateuseraccountComponent, LoginComponent],
 
   exports: [
-    CreatesupplieraccountComponent, CreateuseraccountComponent
+    CreatesupplieraccountComponent, CreateuseraccountComponent, LoginComponent
   ]
 })
 
