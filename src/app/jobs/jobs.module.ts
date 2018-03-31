cbaeeedb4cfecf44ceadf8d09b93f48dc699eae7
Maya Muralidhar <mmuralidhar@smu.edu>
2018-03-31 17:00:15 -0500

    Fixed ng2-date-picker bug

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AddjobsComponent } from './addjobs/addjobs.component';
-import {DpDatePickerModule} from 'ng2-date-picker';
 
 
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule, DpDatePickerModule,
+  imports: [CommonModule, RouterModule, FormsModule,
   ],
   declarations: [AddjobsComponent],
 
