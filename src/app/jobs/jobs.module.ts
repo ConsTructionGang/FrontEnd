214cb924a8d74bf999f0d5376b9816d8cbae435d
Alexandra Ingrando <aingrando@smu.edu>
2018-04-12 17:33:02 -0500

    added user page

--- src/app/jobs/jobs.module.ts
+++ src/app/jobs/jobs.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { AddjobsComponent } from './addjobs/addjobs.component';
 import { ViewJobsComponent } from './view-jobs/view-jobs.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 
 
 @NgModule({
   imports: [CommonModule, RouterModule, FormsModule,
   ],
-  declarations: [AddjobsComponent, ViewJobsComponent],
+  declarations: [AddjobsComponent, ViewJobsComponent, UserPageComponent],
 
-  exports: [AddjobsComponent]
+  exports: [AddjobsComponent, ViewJobsComponent, UserPageComponent]
 })
 
 export class JobsModule { }
