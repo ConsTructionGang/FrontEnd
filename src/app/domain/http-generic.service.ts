c6ae2c86718f7168756e13a6ac9ca8d1a95e89aa
Remus Tumac <rtumac@smu.edu>
2018-04-13 17:32:09 -0500

    Generic Http Service finished

---
+++ src/app/domain/http-generic.service.ts
@@ -0,0 +1,63 @@
+import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+
+export abstract class HttpGeneric<T> {
+
+    protected abstract endPoint;
+
+    private httpOptions = {
+        headers: new HttpHeaders({
+            'Content-Type': 'application/json'
+
+            //needs to be commented out if trying to hit backend server
+            //, 'x-mock-response-code': '200'        //tells mock DB what kind of response to expect
+        }),
+
+        observe: 'response' as 'body'
+    };
+
+    constructor(protected httpClient: HttpClient) { }
+
+    public get(): Observable<T[]> {
+        return this.httpClient.get(`${this.endPoint}`, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    public getById(id: number): Observable<T> {
+        return this.httpClient.get(`${this.endPoint}/${id}`, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    public add(item: T): Observable<T> {
+        return this.httpClient.post(`${this.endPoint}`, item, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    public update(item: T): Observable<T> {
+        return this.httpClient.put(`${this.endPoint}`, item, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    public updateById(id: number, item: T): Observable<T> {
+        return this.httpClient.put(`${this.endPoint}/${id}`, item, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    public delete(id: number): Observable<T> {
+        return this.httpClient.delete(`${this.endPoint}/${id}`, this.httpOptions).pipe(
+            catchError(this.handleException)
+        );
+    }
+
+    protected handleException(exception: any) {
+        var message = `${exception.status} : ${exception.statusText}\r\n${exception.body.error}`;
+        alert(message);
+        return Observable.throw(exception);
+    }
+}
--- src/app/domain/login-http.service.ts
+++ src/app/domain/login-http.service.ts
@@ -1,39 +1,17 @@
-import { catchError } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Account } from '../domain';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
+import { HttpGeneric } from './http-generic.service';
 
 @Injectable()
-export class LoginHttpService {
-
+export class LoginHttpService extends HttpGeneric<any> {
     //mock db
-    // private endPoint = 'https://809ea74f-a447-4e0b-a845-74177a5243a8.mock.pstmn.io';
+    // protected endPoint = 'https://809ea74f-a447-4e0b-a845-74177a5243a8.mock.pstmn.io/login';
 
     //backend server
-    private endPoint = 'http://ec2-34-227-162-95.compute-1.amazonaws.com';
+    protected endPoint = 'http://ec2-34-227-162-95.compute-1.amazonaws.com/login';
 
-    private httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json'
-
-            //needs to be commented out if trying to hit backend server
-            //, 'x-mock-response-code': '200'        //tells mock DB what kind of response to expect
-        }),
-
-        observe: 'response' as 'body'
-    };
-
-    constructor(protected httpClient: HttpClient) { }
-
-    public update(account: any): Observable<HttpResponse<any>> {
-        return this.httpClient.put(`${this.endPoint}/login`, account, this.httpOptions).pipe(
-            catchError(this.handleException)
-        );
-    }
-
-    protected handleException(exception: any) {
-        var message = `${exception.status} : ${exception.statusText}\r\n${exception.body.error}`;
-        alert(message);
-        return Observable.throw(exception);
+    constructor(protected httpClient: HttpClient) {
+        super(httpClient);
     }
 }
